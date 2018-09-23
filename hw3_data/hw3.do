1c19c535f15722f451bb33797a311ec981176bb9
tx58 <tx58@cornell.edu>
2018-09-23 19:40:58 -0400

    published homework

--- hw3_data/hw3.do
+++ hw3_data/hw3.do
@@ -1,5 +1,9 @@
+********************************************************************************
+* Overview of US Economy
+********************************************************************************
 clear
 set more off
+set scheme s2mono, permanently
 local files : dir "C:\Users\13695\Documents\MATLAB\Macro\hw3_data" files "*.csv"
 
 cd "C:\Users\13695\Documents\MATLAB\Macro\hw3_data"
@@ -15,7 +19,8 @@ foreach file in `files' {
 	/* do your stuff here */
 }
 
-* Q1-5: Different Fractions of GDP
+* Q1-3: Different Fractions of GDP
+
 use GDP.dta, clear
 tsset edate
 merge 1:1 edate using PCE.dta, keep(3) nogen
@@ -28,10 +33,58 @@ gen inv_gdp=gpdi/gdp
 gen exp_gdp=gte/gdp
 gen labor_gdp=pincome/gdp
 gen cap_gdp=1-labor_gdp
+summarize *_*
+
 tsline cons_gdp inv_gdp exp_gdp
-graph export gdp_spending_approach.png, width(1200) replace
+graph export "Figure/gdp_spending_approach.png", width(1200) replace
 tsline labor_gdp cap_gdp
-graph export gdp_income_approach.png, width(1200) replace
+graph export "Figure/gdp_income_approach.png", width(1200) replace
+
+keep *_*
+unab a: _all
+foreach k in `a'{
+	gen `k'1=`k'
+	gen `k'2=`k'
+	gen `k'3=`k'
+	gen `k'4=`k'
+}
+collapse (mean)(*1) (sem)(*2) (max)(*3) (min)(*4)
+gen id=_n
+reshape long `a', i(id) j(n)
+tostring id, replace
+replace id="mean" if n==1
+replace id="se" if n==2
+replace id="max" if n==3
+replace id="min" if n==4
+export delimited id *_* using "Table/gdp_share", replace
+
+* Q4-5: Labor share and capital share
+
+use LABSHARE.dta, clear
+tsset edate
+rename labshpusa156nrug labor
+gen cap=1-labor
+tsline labor cap
+graph export "Figure/gdp_income_approach.png", width(1200) replace
+
+keep labor cap
+unab a: _all
+foreach k in `a'{
+	gen `k'1=`k'
+	gen `k'2=`k'
+	gen `k'3=`k'
+	gen `k'4=`k'
+}
+collapse (mean)(*1) (sem)(*2) (max)(*3) (min)(*4)
+gen id=_n
+reshape long `a', i(id) j(n)
+tostring id, replace
+replace id="mean" if n==1
+replace id="se" if n==2
+replace id="max" if n==3
+replace id="min" if n==4
+export delimited id lab cap using "Table/gdp_lk", replace
+
 
 * Q6-7: Growth rate of gdp and consumption
 
@@ -49,20 +102,55 @@ gen d_cons=d.c_capita
 gen r_cons=d_cons/ c_capita
 tsset edate
 tsline r_gdp r_cons
-graph export growth_of_gdp_and_consumption.png, width(1200) replace
+graph export  "Figure/growth_of_gdp_and_consumption.png", width(1200) replace
+keep *_*
+local a "r_gdp r_cons"
+foreach k in `a'{
+	gen `k'1=`k'
+	gen `k'2=`k'
+	gen `k'3=`k'
+	gen `k'4=`k'
+}
+collapse (mean)(*1) (sem)(*2) (max)(*3) (min)(*4)
+gen id=_n
+reshape long `a', i(id) j(n)
+tostring id, replace
+replace id="mean" if n==1
+replace id="se" if n==2
+replace id="max" if n==3
+replace id="min" if n==4
+export delimited id *_* using "Table/gdp_contribution", replace
+
 
 * Q8-9: Unemployment
 
 use UNRATE.dta, clear
 tsset edate
 tsline unrate
-graph export unemployment_rate.png, width(1200) replace
+graph export  "Figure/unemployment_rate.png", width(1200) replace
 
 use UEMPMEAN.dta, clear
 merge 1:1 edate using UEMPMED.dta, keep(3) nogen
 tsset edate
 tsline uempmean uempmed
-graph export unemployment_rate.png, width(1200) replace
+graph export "Figure/unemployment_rate.png", width(1200) replace
+
+local a "uempmean uempmed"
+foreach k in `a'{
+	gen `k'1=`k'
+	gen `k'2=`k'
+	gen `k'3=`k'
+	gen `k'4=`k'
+}
+collapse (mean)(*1) (sem)(*2) (max)(*3) (min)(*4)
+gen id=_n
+reshape long `a', i(id) j(n)
+tostring id, replace
+replace id="mean" if id=="1"
+replace id="se" if id=="2"
+replace id="max" if id=="3"
+replace id="min" if id=="4"
+export delimited id `a' using "Table/unemployment", replace
 
 
 
--- hw3_dp.m
+++ hw3_dp.m
@@ -1,69 +1,84 @@
 % Tianli Xia
 % Macro Homework 3
 % Dynamic programming: value-iteration
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Q(s, k)= (s^{\alpha} - (1-\delta)s - k) + \beta V(k)
-% V(s)= max_{k} Q(s, k)
-% Transition: s=k
-% State variable: s
-% Action varible: k
+% Policy Function:
+%% 
+% $Q(s, k)= log(s^{\alpha} - (1-\delta)s - k) + \beta V(k)$
+
+% Value Function:
+%% 
+% $V(s)= max_{k} Q(s, k)$
+
+%%
+% Other important notations:
 % 
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% * Transition: s=k
+% * State variable: s
+% * Action varible: k
+% 
 
 clear all
 
-%% Initialize the problem
-alpha=0.3;
-beta=0.6;
-delta=0.75;
+%% Initialize the parameters
+
+alpha=0.3; % the production rate: $f(k)=k^{\alpha}$
+beta=0.6; % the intertemporary patience
+delta=0.75; % capital deprecation rate
 k=200; % number of grids
-len= 0.001; % length of grid, it is good to start from big value and then decrease
+len= 0.001; % length of grid, it is good to start from big value and then decrease.
+% In pratice, I set length to be 0.01 at the beginning, at find that the
+% optimal value at steady state should be within (0, 0.2), hence I set the
+% length to a lower value to cover this interval.
 start= len;
-state= start:len:start+len*(k-1); % different states:
+state= start:len:start+len*(k-1); % different states in grids:
 action= start:len:start+len*(k-1);
 
 %% Initialize value function matrix
 V = ones(k,1); % V: state* action matrix
 PI = ones(k,1); % PI: best policy matrix
 threshold= 10^(-6); % Tolerance level in the loop
-epsilon=1; 
+epsilon=1; % random initial value of the gao between two loops
 count=0; % Count how many times the loop runs
 
-while epsilon> threshold
-    V_temp=-inf*ones(k);
+while epsilon> threshold % loop until $\epsilon$ converges
+    V_temp=-inf*ones(k); % Any infeasible capital brings -inf value
     for s=1:k % value function iteration: get current value
-        a_max=state(s)^alpha+ (1-delta)*state(s); % Calculate feasible action sets: 0<=a<=state(s)^alpha+ (1-delta)*state(s)
+        a_max=state(s)^alpha+ (1-delta)*state(s); % Calculate feasible action sets: $0<=a<=state(s)^alpha+ (1-delta)*state(s)$
         for a=1:min(k, ceil( (a_max-start)/len) )
             V_temp(s,a)= log( state(s)^alpha+ (1-delta)*state(s) -action(a)) + beta* V(a); 
+            % This directly comes from Bellman Optimality equation
         end
         [V_new, PI]= max(V_temp, [], 2); % Calculate (1) New value function; (2) best action function.
     end
-    epsilon= ( max( abs(V_new -V))) % Calculate the current error
+    epsilon= ( max( abs(V_new -V))); % Calculate the current error
     V=V_new;
     count=count+1; % Count times the loop ends
 end
 
+fprintf("The loop ends in %d runs, the gap within final two loops are %f.", count, epsilon)
+
 %% Plot graph
-% plot the value function
+%% Plot the value function
+% $V(k)= \max_{k} log(s^{\alpha} - (1-\delta)s - k) + \beta V(k)$
 plot(state, V)
 grid on
 axis on
 xlabel("Current capital")
 ylabel("Value")
 title("Value Function on each state")
-print -djpeg -r600 hw3_value_2
-
  
-%plot potential function of actions at each state 
-for i=1:10:51
+% print -djpeg -r600 hw3_value_2
+ 
+%% Plot potential function of actions in terms of action at each state 
+% $Q(s,a)= \max_{k} log(s^{\alpha} - (1-\delta)s - k) + \beta V(k)$
+for i=1:30:151
     plot(state, V_temp(i,:))
     hold on
 end
 xlabel("Next period capital decision")
 ylabel("Optimality function q(s,a)")
-lgd= legend("0.001","0.011","0.021","0.031","0.041","0.051")
-title(lgd, "Current Captial")
+lgd= legend("0.001","0.031","0.061","0.091","0.121","0.151",'Location','southeast');
+title(lgd, "Current State (Captial)")
 axis on
 grid on
 hold off
@@ -72,20 +87,34 @@ hold off
 %     hold on
 % end
 % hold off
-print -djpeg -r600 hw3_control_2
+%print -djpeg -r600 -hw3_control_2
 
 
-%plot action function
+%% Plot action function
+%This plots show the optimal capital decision. At any point on the left side
+%of steady state, we increase the capital and vice versa. At steady state
+%it is stable. Hence the steady state is the crosspoint of the policy
+%function and the 45 degree line.
+i=1;
+while i~=PI(i) % By definition, this is the optimal decision
+    i=i+1;
+end
+      
+fprintf("The optimal capital at the steady state is %f\n. The value at optimal capital is %f\n" ...
+    , i*len, V(i) )
+fprintf("If feasible, the planner will always choose steady state capital in the next period.")
     plot(state, action(PI))
     x=state;
     y=state;
     hold on
     plot(x,y)
+    hold on
+    plot(x(i),y(i),'r*')
     grid on
     axis on
     xlabel("Current capital")
     ylabel("Next period best action")
-    title("Action Function on each state")
+    title("Policy Function on each state")
     axis square
-    print -djpeg -r600 hw3_action_2
+%   print -djpeg -r600 hw3_action_2
 
